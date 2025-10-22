5df3f8ada6b3e8e4634d1989bbb684ffd73b6a81
Ömer Ali Uygun <112541841+omerali1996@users.noreply.github.com>
2025-10-22 21:47:46 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-import { BACKEND_URL } from "./config";
+
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
 
 const api = axios.create({
   baseURL: BACKEND_URL,
-  timeout: 10000
+  timeout: 10000,
 });
 
 export default api;
